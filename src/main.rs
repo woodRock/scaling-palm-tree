e827414cefc11eb689a097bf6ae8a461863b05d5
Jesse Wood <j.r.h.wood98@gmail.com>
2022-11-08 22:22:45 +1300

    Pass $ clippy --all -- -D warnings

--- src/main.rs
+++ src/main.rs
@@ -35,11 +35,11 @@ async fn get_departure_board(stop_id: String) -> Result<Value, Box<dyn std::erro
         reqwest::StatusCode::OK => {
             let body: String = resp.text().await?;
             let v: Value = serde_json::from_str(&body)?;
-            return Ok(v);
-        }
+            Ok(v)
+        },
         reqwest::StatusCode::FORBIDDEN => {
             panic!("Forbidden. Please check your API key.");
-        }
+        },
         _ => {
             panic!(
                 "Unknown error: {}\nPlease check if service id {} exists.",
@@ -47,47 +47,47 @@ async fn get_departure_board(stop_id: String) -> Result<Value, Box<dyn std::erro
                 stop_id
             );
         }
-    };
+    }
 }
 
 /// Formats the departure string for a service.
 fn pretty_format(departure: &serde_json::Value, service_id: &String) -> String {
     let destination = departure["destination"]["name"].as_str().unwrap();
     let mut print_str = format!("{}\t{}\t", service_id, destination);
-    let departure_time;
     // When buses are far away, the only estimate is the scheduled departure time.
-    if departure["arrival"]["expected"].is_null() {
-        departure_time = departure["arrival"]["aimed"].as_str().unwrap();
+    let departure_time: &str = if departure["arrival"]["expected"].is_null() {
+        departure["arrival"]["aimed"].as_str().unwrap()
     // When buses are close to arrivial, the API can return expected time calculate via their GPS location.
     } else {
-        departure_time = departure["arrival"]["expected"].as_str().unwrap();
+        departure["arrival"]["expected"].as_str().unwrap()
     };
     // Some buses are cancelled, minutes to arrival is replaced with a cancellation notice.
     if departure["status"] == "cancelled" {
         print_str.push_str("CAN\t");
-        return print_str;
-    };
-    // Caculate minutes to arrival in minutes.
-    let now = chrono::Local::now();
-    let departure_time = chrono::DateTime::parse_from_rfc3339(departure_time).unwrap();
-    let minutes = departure_time.signed_duration_since(now).num_minutes();
-    // Buses less than minutes away are due.
-    if minutes < 0 {
-        print_str.push_str("Due\t");
-    // Buses more than an hour away are given in time, not hours.
-    } else if minutes > 60 {
-        let time_format = format!("{}\t", departure_time.format("%H:%M%p"));
-        print_str.push_str(&time_format);
-    // All other buses are given in minutes to arrival.
+        print_str
     } else {
-        let minutes_format = format!("{}min\t", minutes);
-        print_str.push_str(&minutes_format);
-    };
-    // Provide a wheelchair access indicator for accesibility.
-    if departure["wheelchair_accessible"] == true {
-        print_str.push('♿');
-    };
-    return print_str;
+        // Caculate minutes to arrival in minutes.
+        let now = chrono::Local::now();
+        let departure_time = chrono::DateTime::parse_from_rfc3339(departure_time).unwrap();
+        let minutes = departure_time.signed_duration_since(now).num_minutes();
+        // Buses less than minutes away are due.
+        if minutes < 0 {
+            print_str.push_str("Due\t");
+        // Buses more than an hour away are given in time, not hours.
+        } else if minutes > 60 {
+            let time_format = format!("{}\t", departure_time.format("%H:%M%p"));
+            print_str.push_str(&time_format);
+        // All other buses are given in minutes to arrival.
+        } else {
+            let minutes_format = format!("{}min\t", minutes);
+            print_str.push_str(&minutes_format);
+        };
+        // Provide a wheelchair access indicator for accesibility.
+        if departure["wheelchair_accessible"] == true {
+            print_str.push('♿');
+        };
+        print_str
+    }   
 }
 
 #[tokio::main]
